13057c176ac807520cadceac960dba46aa95b92e
Adam Nelson <adamnelson@mines.edu>
2018-12-02 16:23:39 -0700

    Program is complete and properly generates random solutions.

--- 8rooks.js
+++ 8rooks.js
@@ -1,19 +1,16 @@
 function runDemo() {
     var board = {
       	rooks : [], // Stores the rook objects
-      	occupiedSpots : [], // Stores the occupied spaces on the board
       	placeRook : function(location) {
       		var rook = {row:location.substr(0,1), col:location.substr(1, 1)};
-			document.getElementById(location).innerHTML = "R";
 			this.rooks.push(rook);
       	},
       	removeRook : function(location) {
       		var index = 0;
       		for (i = 0; i < this.rooks.length; i++) {
-      			if (array[i].row = location.substr(0, 1)) {
-					if (array[i].col = location.substr(1, 1)) {
+      			if (this.rooks[i].row = location.substr(0, 1)) {
+					if (this.rooks[i].col = location.substr(1, 1)) {
 						index = i;
-						document.getElementById(location).innerHTML = "-";
 					}
 				}
       		}
@@ -22,44 +19,36 @@ function runDemo() {
       	validate : function() {
       		var temp = checkBoard(this.rooks);
 			if (temp.row == 0 && temp.col == 0) {
-				//return true;
-				window.alert("Board is valid");
+				return true;
 			} else {
-				//return false;
-				window.alert("Board is not valid");
+				return false;
 			}
+      	},
+      	fillBoard : function() {
+      		var valid = false;
+      		while (valid == false) {
+  				var spot = randSpot();
+  				this.placeRook(spot);
+  				if (this.validate() == false) this.removeRook(spot);
+      			if (this.rooks.length == 8 && this.validate()) valid = true;
+      		}
       	}
 	};
 
-	//fillBoard(board);
-	board.placeRook("a1");
-	board.placeRook("a2");
-	board.validate();
-
+	var spots = [
+	"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", 
+	"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", 
+	"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", 
+	"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", 
+	"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", 
+	"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", 
+	"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", 
+	"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
+	// Reset board with each button click
+	for (i = 0; i < spots.length; i++) document.getElementById(spots[i]).innerHTML = "-";
+	board.fillBoard();
 	printInfo(board.rooks);
 }
-
-
-
-// function placeRook(array, location) {
-// 	var rook = {row:location.substr(0,1), col:location.substr(1, 1)};
-// 	document.getElementById(location).innerHTML = "R";
-// 	array.push(rook);
-// 	return array;
-// }
-
-// function removeRook(array, location) {
-// 	var index = 0;
-// 	for (i = 0; i < array.length; i++) {
-// 		if (array[i].row = location.substr(0, 1)) {
-// 			if (array[i].col = location.substr(1, 1)) {
-// 				index = i;
-// 				document.getElementById(location).innerHTML = "-";
-// 			}
-// 		}
-// 	}
-// 	return array.splice(index, 1);
-// }
 
 function checkBoard(array) {
 	var occupiedRows = [];
@@ -78,15 +67,6 @@ function checkBoard(array) {
 		occupiedCols.push(array[i].col);
 	}
 	return valid;
-}
-
-function validate(array) {
-	var temp = checkBoard(array);
-	if (temp.row == 0 && temp.col == 0) {
-		return true; // window.alert("Board is valid");
-	} else {
-		return false; // window.alert("Board is not valid");
-	}
 }
 
 function randSpot() {
@@ -112,4 +92,12 @@ function printInfo(array) {
 	if (array.length > 5) document.getElementById("R6").innerHTML = "R6: " + array[5].row + array[5].col;
 	if (array.length > 6) document.getElementById("R7").innerHTML = "R7: " + array[6].row + array[6].col;
 	if (array.length > 7) document.getElementById("R8").innerHTML = "R8: " + array[7].row + array[7].col;
+	document.getElementById(array[0].row + array[0].col).innerHTML = "R";
+	document.getElementById(array[1].row + array[1].col).innerHTML = "R";
+	document.getElementById(array[2].row + array[2].col).innerHTML = "R";
+	document.getElementById(array[3].row + array[3].col).innerHTML = "R";
+	document.getElementById(array[4].row + array[4].col).innerHTML = "R";
+	document.getElementById(array[5].row + array[5].col).innerHTML = "R";
+	document.getElementById(array[6].row + array[6].col).innerHTML = "R";
+	document.getElementById(array[7].row + array[7].col).innerHTML = "R";
 }
